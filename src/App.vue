2484dfe6b8b816772d9dbfd8f604a7b3f0edb204
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-08-12 23:32:09 +0500

    addd

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <product/>
+  <total-price/>
+</template>
+<script setup>
+
+import Product from "./components/product.vue";
+import TotalPrice from "./components/totalPrice.vue";
+</script>
---
+++ src/components/product.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex grid grid-cols-4 gap-6 m-6">
+    <product-one/>
+    <product-two/>
+    <product-three/>
+    <product-four/>
+  </div>
+</template>
+<script setup>
+
+import ProductOne from "./products/productOne.vue";
+import ProductTwo from "./products/productTwo.vue";
+import ProductThree from "./products/productThree.vue";
+import ProductFour from "./products/productFour.vue";
+</script>
