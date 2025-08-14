f3a17d8bf31a11ef6bbea78c9c6eb7f8d4a233d0
umidsamadov0927 <umidsamadov0927@gmail.com>
2025-08-14 18:28:55 +0500

    added

---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <div class="flex justify-between ml-[15px] mt-[50px] mb-[100px] items-center header ">
+      <div class="flex  items-center gap-[50px]">
+        <img src="../assets/brotherPhotos/herader.svg" alt="header">
+        <p class="font-[400] none text-[18px]">Home</p>
+        <p class="font-[400] none text-[18px]">Collections</p>
+        <p class="font-[400] none text-[18px]">New</p>
+      </div>
+      <div>
+        <img class="w-[30px] responsiveImg h-[30px] hidden" src="../assets/brotherPhotos/responsive.svg" alt="responsive">
+      </div>
+      <div class="flex">
+        <div class="flex none items-center gap-[50px]">
+          <img src="../assets/brotherPhotos/bigHeart.svg" alt="bigHearts">
+          <div class="w-[75px] h-[50px] rounded-[22px] bg-[#000000] text-[#FFFFFF] pt-[11px] text-center">Cart</div>
+        </div>
+        <div class="flex resGap items-center ml-[-5px] gap-[50px]">
+          <img src="../assets/brotherPhotos/header4.svg" alt="bigHearts">
+          <img src="../assets/brotherPhotos/header5.svg" alt="bigHearts">
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script setup>
+
+</script>
--- src/components/product.vue
+++ src/components/product.vue
@@ -1,15 +1,89 @@
 <template>
-  <div class="flex grid grid-cols-4 gap-6 m-6">
-    <product-one/>
-    <product-two/>
-    <product-three/>
-    <product-four/>
+  <div class="flex  m-4 gap-10 product">
+    <div v-for="(product,index) in productData" :key="index">
+      <div class="flex gap-4">
+        <div>
+          <img :src="product.productPhoto" alt="photos">
+          <div @click="toggleLike(index)" class="w-[36px] absolute cursor-pointer mt-[-36px] ml-[223px] h-[36px] bg-white">
+            <img v-show="!product.isLiked" class="absolute mt-[11px] ml-[11px] w-[13px] h-[13px] rotate-45 " src="../assets/brotherPhotos/heart.svg" alt="heart">
+            <img v-show="product.isLiked" class="absolute mt-[11.8px] ml-[10.5px] w-[14px] h-[14px] " src="../assets/brotherPhotos/heart2.png" alt="heart2">
+          </div>
+          <p>{{product.productName}}</p>
+          <div class="flex gap-[60px]">
+            <p>{{product.productTitle}}</p>
+            <p>$ {{ product.productMoney * product.productData }}</p>
+          </div>
+        </div>
+        <div>
+          <img class="w-[12px] ml-1 h-[12px] mb-[60px]" src="../assets/brotherPhotos/exit.svg" alt="exit">
+          <p class="font-[500] text-[14px] mb-[15px]">L</p>
+          <div class="w-[27px] h-[27px] mb-[15px] bg-[#1B1B1D]"></div>
+          <button @click="plus(index)" class="w-[27px] h-[27px] border text-[17px] border-[#8A8A8A]">+</button>
+          <p class="w-[27px] h-[27px] border border-[#8A8A8A] font-[500] text-center text-[15px]">{{product.productData}}</p>
+          <button @click="minus(index)" class="w-[27px] h-[27px] border text-[17px] mb-[25px] border-[#8A8A8A]">-</button>
+          <img class="ml-1" src="../assets/brotherPhotos/replace.svg" alt="replace">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
+import {ref} from "vue";
 
-import ProductOne from "./products/productOne.vue";
-import ProductTwo from "./products/productTwo.vue";
-import ProductThree from "./products/productThree.vue";
-import ProductFour from "./products/productFour.vue";
+let emit = defineEmits(["totalPrice"]);
+
+function allPrice() {
+  const total = productData.value.reduce((sum, item) => {
+    return sum + (item.productMoney * item.productData)
+  }, 0)
+  emit("totalPrice", total)
+}
+
+
+let isLiked = ref(false)
+
+function plus(index) {
+  if (productData.value[index].productData < 50) {
+    productData.value[index].productData++;
+    allPrice()
+  }
+}
+
+function minus(index) {
+  if (productData.value[index].productData > 1) {
+    productData.value[index].productData--;
+    allPrice()
+  }
+}
+function toggleLike(index) {
+  productData.value[index].isLiked = !productData.value[index].isLiked;
+}
+
+import img from "../assets/brotherPhotos/blackBro.png"
+import img2 from "../assets/brotherPhotos/whiteBro.png"
+let productData = ref([
+  {
+    productPhoto:img2,
+    productName:"Cotton T Shirt",
+    productTitle:"Full Sleeve Zipper",
+    productMoney: 99,
+    productData: 1
+
+  },
+  {
+    productPhoto:img,
+    productName:"Cotton T Shirt",
+    productTitle:"Basic Slim Fit T-Shirt",
+    productMoney: 99,
+    productData: 1
+  },
+  {
+    productPhoto:img2,
+    productName:"Cotton T Shirt",
+    productTitle:"Full Sleeve Zipper",
+    productMoney: 99,
+    productData: 1
+  }
+])
+
 </script>
